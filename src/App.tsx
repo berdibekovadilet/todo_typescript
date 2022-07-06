4cc48527a77a00e30c68a341033307c84cbf3586
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-06 22:25:14 +0600

    added todoTask component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
-import { FC } from "react";
+import { FC, ChangeEvent, useState } from "react";
+import { ITask } from "./interfaces";
 import "./App.css";
+import TodoTask from "./components/TodoTask";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadline(0);
+  };
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Что сделать?" />
-          <input type="number" placeholder="За какой срок сделать" />
+          <input
+            type="text"
+            placeholder="Что сделать?"
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="За какой срок сделать"
+            name="deadline"
+            value={deadline}
+            onChange={handleChange}
+          />
         </div>
-        <button>Добавить задачу</button>
+        <button onClick={addTask}>Добавить задачу</button>
       </div>
-      <div className="todoList"></div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} />;
+        })}
+      </div>
     </div>
   );
 };
